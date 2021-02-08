033218b7ac73a71a576c796948e41dd7ab4f019c
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2021-02-08 20:27:20 +0100

    Add files via upload

---
+++ kostroEugenLikovi/kostroEugenLikovi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenLikovi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cetverokut cetverokut = new Cetverokut();
+            cetverokut.UcitajStranice();
+            double OCetverokut = cetverokut.Opseg();
+            Console.WriteLine("Opseg zadanog cetverokuta je: " + OCetverokut);
+            Trokut trokut = new Trokut();
+            trokut.UcitajStranice();
+            double OTrokut = trokut.Opseg();
+            Console.WriteLine("Opseg zadanog trokuta je: " + OTrokut);
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
